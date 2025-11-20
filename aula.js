fca592dce8801059edb70eadf853d3f832fa2b84
anab1231 <programadorsistemas0@gmail.com>
2025-11-19 21:40:34 -0300

    AULA2

--- aula.js
+++ aula.js
@@ -10,8 +10,50 @@ const menuIcon = menuBtn.querySelector('i');
 // o primeiro elemento que corresponde a um seletor CSS dentro de outro elemento.
 
 // --------------------------------------------------------------------
+// Adiciona um "ouvinte de evento" (listener) que detecta o clique no botão do menu.
+menuBtn.addEventListener('click', () => {
+  // addEventListener() -> método reservado usado para executar uma função
+  // toda vez que um evento específico ocorre (nesse caso, um clique).
+
+  // Alterna (liga/desliga) a classe "show" no menu de navegação.
+  // Isso faz o menu aparecer ou desaparecer visualmente via CSS.
+  navMenu.classList.toggle('show');
+ // Troca o ícone (de hambúrguer para "X" e vice-versa), dependendo do estado atual.
+  // Verifica se o ícone atual tem a classe "fa-bars" (ícone de hambúrguer)
+  if (menuIcon.classList.contains('fa-bars')) {
+    // classList.contains() -> verifica se o elemento contém uma classe específica (retorna true ou false).
+
+    // Se sim, remove a classe do ícone de hambúrguer...
+    menuIcon.classList.remove('fa-bars');
+    // ...e adiciona o ícone de "X" para indicar que o menu pode ser fechado.
+    menuIcon.classList.add('fa-xmark');
+  } else {
+    // Caso contrário, volta o ícone para o estado original (hambúrguer).
+    menuIcon.classList.remove('fa-xmark');
+    menuIcon.classList.add('fa-bars');
+  }
+});
+
+  // classList.toggle() -> adiciona a classe se ela não existir; remove se já existir.
+
+ const tryed = new typed (".auto-type", {
+strings :["JK FINANCE", "INVESTINDO NOS SEUS SENHOS"],
+typeSpeed: 80,         /*velocidade da digitação*/
+backSpeed: 60,        /*velocidade da remoção do texto */ 
+backdelay: 1200,     /*pausa antes de apagar*/
+loop: true,         /*ativando a repetição*/
+showcursor: false, /*oculta o cursor de digitação*/
+
+ });
 
 
+
+
+
+
+
+
+ 
 
 
 
